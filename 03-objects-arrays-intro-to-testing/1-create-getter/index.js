df774ff86bb3f950e00359f3481e4d819e2ac4b4
Dimoon <dvpashnev@gmail.com>
2021-11-16 17:44:50 +0200

    03-1 & 03-2 tasks have been solved

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,18 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  return function (obj) {
+    const parts = path.split('.');
+    let resultProp;
+    for (let prop of parts) {
+      if (obj[prop]) {
+        resultProp = obj[prop];
+        obj = obj[prop];
+      } else {
+        resultProp = undefined;
+        break;
+      }
+    }
+    return resultProp;
+  };
 }
