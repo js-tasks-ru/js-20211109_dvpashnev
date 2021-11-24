dab067dec4a863a091fb7c166a59ec0b1df91da6
Dimoon <dvpashnev@gmail.com>
2021-11-24 15:51:31 +0200

    05-1 task has been solved

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,66 @@
 export default class NotificationMessage {
+  static element;
+  element = NotificationMessage.element;
 
+  constructor(
+    note = 'Hello World',
+    {
+      duration = 200,
+      type = 'success'
+    } = {}) {
+    this.note = note;
+    this.duration = duration;
+    this.type = type;
+
+    this.render();
+    this.initEventListeners();
+  }
+
+  get template() {
+    return `
+    <div class="notification ${this.type}" style="--value:${(this.duration/1000).toFixed()}s">
+    <div class="timer"></div>
+    <div class="inner-wrapper">
+      <div class="notification-header">${this.type}</div>
+      <div class="notification-body">
+        ${this.note}
+      </div>
+    </div>
+  </div>
+    `;
+  }
+
+  render() {
+    if (NotificationMessage.element) {
+      this.destroy();
+    }
+    const element = document.createElement('div');
+
+    element.innerHTML = this.template;
+
+    NotificationMessage.element = element.firstElementChild;
+    this.element = NotificationMessage.element;
+
+    setTimeout(() => this.destroy(), this.duration);
+  }
+
+  show(targetElement = document.body) {
+    this.render();
+    targetElement.append(this.element);
+  }
+
+  initEventListeners() {
+
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+  }
 }
