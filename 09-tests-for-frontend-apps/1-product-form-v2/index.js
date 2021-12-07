3d1a4a5cc5a8872556c2c9df0f2dbc53a9a770ca
Dimoon <dvpashnev@gmail.com>
2021-12-08 00:24:05 +0200

    9th module tasks have been solved

--- 09-tests-for-frontend-apps/1-product-form-v2/index.js
+++ 09-tests-for-frontend-apps/1-product-form-v2/index.js
@@ -1,4 +1,4 @@
-import SortableList from '../../2-sortable-list/src/index.js';
+import SortableList from '../2-sortable-list/index.js';
 import escapeHtml from './utils/escape-html.js';
 import fetchJson from './utils/fetch-json.js';
 
@@ -6,11 +6,290 @@ const IMGUR_CLIENT_ID = '28aaa2e823b03b1';
 const BACKEND_URL = 'https://course-js.javascript.ru';
 
 export default class ProductForm {
-  constructor (productId) {
-    this.productId = productId;
+
+  element;
+  product = {};
+  categoriesNSubcategories = [];
+  subElements = {};
+  fields = {};
+
+  defaultFormData = {
+    title: '',
+    description: '',
+    quantity: 1,
+    subcategory: '',
+    status: 1,
+    images: [],
+    price: 100,
+    discount: 0
+  };
+
+  onImageInputClick = () => {
+    const fileInput = document.createElement("input");
+    fileInput.type = "file";
+    fileInput.accept = "image/*";
+    fileInput.addEventListener('change', this.onImageInputChange);
+
+    this.subElements.fileInput = fileInput;
+
+    fileInput.click();
+  };
+
+  onImageInputChange = async () => {
+    this.subElements.uploadImage.classList.add('is-loading');
+    try {
+      const [file] = this.subElements.fileInput.files;
+      const result = await this.upload(file);
+
+      this.addImage(this.renderImage(result.data.link, file.name));
+    } catch (error) {
+      console.error(error);
+    } finally {
+      this.subElements.uploadImage.classList.remove('is-loading');
+    }
+  };
+
+  onSubmit = async (event) => {
+    event.preventDefault();
+
+    await this.save();
   }
 
-  async render () {
+  constructor(productId) {
+    this.productId = productId;
 
+    this.categoriesNSubcategoriesUrl = new URL(`api/rest/categories`, BACKEND_URL);
+    this.productUrl = new URL(`api/rest/products`, BACKEND_URL);
+    this.productUrlWithId = new URL(`api/rest/products`, BACKEND_URL);
+  }
+
+  async loadCategoriesNSubcategories() {
+    this.categoriesNSubcategoriesUrl.searchParams.set('_sort', 'weight');
+    this.categoriesNSubcategoriesUrl.searchParams.set('_refs', 'subcategory');
+    return fetchJson(this.categoriesNSubcategoriesUrl);
+  }
+
+  getCategoriesOptions(data) {
+    return data
+      .map(category => category.subcategories
+        .map(subcategory => `<option value="${subcategory.id}" ${this.product.subcategory === subcategory.id ? 'selected' : ''}>${category.title} &gt; ${subcategory.title}</option>`)
+        .join(''))
+      .join('');
+  }
+
+  async upload(file) {
+    const formData = new FormData();
+
+    formData.append('image', file);
+
+    try {
+      const response = await fetch('https://api.imgur.com/3/image', {
+        method: 'POST',
+        headers: {
+          Authorization: `Client-ID ${IMGUR_CLIENT_ID}`
+        },
+        body: formData,
+        referrer: ''
+      });
+
+      return await response.json();
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+
+  addImage(image) {
+    const ul = this.subElements.imageListContainer.firstElementChild;
+    ul.append(image);
+  }
+
+  renderImage(url = '', source = '') {
+    const element = document.createElement('li');
+    element.className = 'products-edit__imagelist-item sortable-list__item';
+    element.style = '';
+    element.innerHTML = `<input type="hidden" name="url" value="${url}">
+             <input type="hidden" name="source" value="${source}">
+              <span>
+                <img src="icon-grab.svg" data-grab-handle="" alt="grab">
+                <img class="sortable-table__cell-img" alt="Image" src="${url}">
+                <span>${source}</span>
+              </span>
+              <button type="button">
+                <img src="icon-trash.svg" data-delete-handle="" alt="delete">
+              </button>`;
+
+    return element;
+  }
+
+  get template() {
+    return `<div class="product-form">
+    <form data-element="productForm" class="form-grid">
+      <div class="form-group form-group__half_left">
+        <fieldset>
+          <label class="form-label">Название товара</label>
+          <input required="" type="text" name="title" id="title"
+          class="form-control" placeholder="Название товара"
+          value="${this.product.title ? escapeHtml(this.product.title) : ''}" />
+        </fieldset>
+      </div>
+      <div class="form-group form-group__wide">
+        <label class="form-label">Описание</label>
+        <textarea required="" class="form-control" name="description" id="description"
+          data-element="productDescription" placeholder="Описание товара">${this.product.description ? escapeHtml(this.product.description) : ''}</textarea>
+      </div>
+      <div class="form-group form-group__wide" data-element="sortable-list-container">
+        <label class="form-label">Фото</label>
+        <div data-element="imageListContainer">
+          </div>
+        <button type="button" name="uploadImage" class="button-primary-outline">
+          <span>Загрузить</span>
+        </button>
+      </div>
+      <div class="form-group form-group__half_left">
+        <label class="form-label">Категория</label>
+        <select class="form-control" name="subcategory" id="subcategory">
+          ${this.getCategoriesOptions(this.categoriesNSubcategories)}
+        </select>
+      </div>
+      <div class="form-group form-group__half_left form-group__two-col">
+        <fieldset>
+          <label class="form-label">Цена ($)</label>
+          <input required="" type="number" name="price" id="price" class="form-control" placeholder="100"
+          value="${this.product.price ? this.product.price : ''}">
+        </fieldset>
+        <fieldset>
+          <label class="form-label">Скидка ($)</label>
+          <input required="" type="number" name="discount" id="discount" class="form-control" placeholder="0"
+          value="${this.product.discount ? this.product.discount : ''}">
+        </fieldset>
+      </div>
+      <div class="form-group form-group__part-half">
+        <label class="form-label">Количество</label>
+        <input required="" type="number" class="form-control" name="quantity" id="quantity" placeholder="1"
+          value="${this.product.quantity ? this.product.quantity : ''}">
+      </div>
+      <div class="form-group form-group__part-half">
+        <label class="form-label">Статус</label>
+        <select class="form-control" name="status" id="status">
+          <option value="1" ${this.product.status === '1' ? 'selected' : ''}>Активен</option>
+          <option value="0" ${this.product.status === '0' ? 'selected' : ''}>Неактивен</option>
+        </select>
+      </div>
+      <div class="form-buttons">
+        <button type="submit" name="save" class="button-primary-outline">
+          Сохранить товар
+        </button>
+      </div>
+    </form>`;
+  }
+
+  async render() {
+    const categoriesNSubcategoriesPromise = this.loadCategoriesNSubcategories();
+    this.productUrlWithId.searchParams.set('id', this.productId);
+
+    const productPromise = this.productId ? fetchJson(this.productUrlWithId) : [this.defaultFormData];
+
+    const [categoriesNSubcategories, product] = await Promise.all([categoriesNSubcategoriesPromise, productPromise]);
+    [this.product] = product;
+    this.categoriesNSubcategories = categoriesNSubcategories;
+    const element = document.createElement('div');
+
+    element.innerHTML = this.template;
+
+    this.element = element.firstElementChild;
+
+    this.subElements = this.getSubElements(this.element);
+    this.fields = this.getFields(this.element);
+
+    this.sortableList = new SortableList({
+      items: this.product.images.map(image => this.renderImage(image.url, image.source))
+    });
+
+    this.subElements.imageListContainer.append(this.sortableList.element);
+    this.initEventListeners();
+    document.body.append(this.element);
+  }
+
+  getSubElements(element) {
+    const result = {};
+    const elements = element.querySelectorAll('[data-element]');
+
+    for (const subElement of elements) {
+      const name = subElement.dataset.element;
+
+      result[name] = subElement;
+    }
+
+    result.imageListContainer
+      = result['sortable-list-container'].querySelector('[data-element="imageListContainer"]');
+
+    result.uploadImage = this.element.querySelector('[name="uploadImage"]');
+
+    return result;
+  }
+
+  getFields(element) {
+    const result = {};
+    const fields = element.querySelectorAll('[id]');
+
+    for (const field of fields) {
+      result[field.id] = field;
+    }
+
+    return result;
+  }
+
+  initEventListeners() {
+    this.subElements.uploadImage.addEventListener('click', this.onImageInputClick);
+    this.element.addEventListener('submit', this.onSubmit);
+  }
+
+  async save() {
+    const images = [];
+    Object.entries(this.subElements.imageListContainer.firstElementChild.children)
+      .map((elem) => {
+        const inputs = elem[1].querySelectorAll('input');
+
+        if (inputs.length) {
+          images.push({url: inputs[0].value, source: inputs[1].value});
+        }
+      });
+
+    for (const [field, element] of Object.entries(this.fields)) {
+      this.product[field]
+        = element.type === 'number' || field === 'status'
+          ? parseInt(element.value)
+          : escapeHtml(element.value);
+    }
+
+    this.product.images = images;
+
+    try {
+      const response = await fetch(this.productUrl.toString(), {
+        method: this.product.id ? "PATCH" : "PUT",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(this.product),
+        referrer: ''
+      });
+
+      this.element.dispatchEvent(new CustomEvent(this.product.id ? 'product-updated' : 'product-saved',
+        {
+          bubbles: true,
+        }));
+
+      return await response.json();
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+
+  remove () {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
   }
 }
--- 09-tests-for-frontend-apps/2-sortable-list/index.js
+++ 09-tests-for-frontend-apps/2-sortable-list/index.js
@@ -2,79 +2,84 @@ export default class SortableList {
   items;
   subElements = {};
 
-  onPointerDown = (event) => {
-    const li = event.target.closest('.sortable-list__item');
-    if (!li || !Object.values(this.subElements).find(el => el === li)) {
-      return false;
-    }
+  onPointerDown = event => {
+    if (event.target.dataset.grabHandle === '') {
+      const li = event.target.closest('.sortable-list__item')
+          ?? event.target.parentElement.closest('.sortable-list__item');
 
-    let droppableBelow = null;
-    const liBoundingClientRect = li.getBoundingClientRect();
+      console.log(li);
+      console.log(event.target);
+      if (!li) {
+        return false;
+      }
 
-    let placeHolder = document.createElement('div');
-    placeHolder.className = 'sortable-list__placeholder';
-    placeHolder.style.width = liBoundingClientRect.width + 'px';
-    placeHolder.style.height = liBoundingClientRect.height + 'px';
-    placeHolder.style.top = liBoundingClientRect.top + 'px';
-    placeHolder.style.left = liBoundingClientRect.left + 'px';
-    li.after(placeHolder);
+      let droppableBelow = null;
+      const liBoundingClientRect = li.getBoundingClientRect();
 
-    const shiftX = event.clientX - liBoundingClientRect.left;
-    const shiftY = event.clientY - liBoundingClientRect.top;
-    const width = liBoundingClientRect.width;
-    li.style.position = 'absolute';
-    li.style.display = 'flex';
-    li.style.width = width + 'px';
-    li.style.zIndex = 10000;
+      let placeHolder = document.createElement('div');
+      placeHolder.className = 'sortable-list__placeholder';
+      placeHolder.style.width = liBoundingClientRect.width + 'px';
+      placeHolder.style.height = liBoundingClientRect.height + 'px';
+      placeHolder.style.top = liBoundingClientRect.top + 'px';
+      placeHolder.style.left = liBoundingClientRect.left + 'px';
+      li.before(placeHolder);
 
-    moveAt(event.pageX, event.pageY);
+      const shiftX = event.clientX - liBoundingClientRect.left;
+      const shiftY = event.clientY - liBoundingClientRect.top;
+      const width = liBoundingClientRect.width;
+      li.classList.add('sortable-list__item_dragging');
+      li.style.width = width + 'px';
+      this.element.append(li);
 
-    function moveAt(pageX, pageY) {
-      li.style.left = pageX - shiftX + 'px';
-      li.style.top = pageY - shiftY + 'px';
-    }
-
-    function onPointerMove(event) {
       moveAt(event.clientX, event.clientY);
-      li.style.display = 'none';
-      let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
-      li.style.display = 'flex';
 
-      if (!elemBelow) {
-        return;
+      function moveAt(pageX, pageY) {
+        li.style.left = pageX - shiftX + 'px';
+        li.style.top = pageY - shiftY + 'px';
       }
 
-      droppableBelow = elemBelow.closest('.sortable-list__item');
-      if (droppableBelow) {
-        enterDroppable(droppableBelow);
+      function onPointerMove(event) {
+        moveAt(event.clientX, event.clientY);
+        li.style.display = 'none';
+        let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+        li.style.display = 'flex';
+
+        if (!elemBelow) {
+          return;
+        }
+
+        droppableBelow = elemBelow.closest('.sortable-list__item');
+        if (droppableBelow) {
+          enterDroppable(droppableBelow);
+        }
       }
-    }
 
-    document.addEventListener('pointermove', onPointerMove);
+      document.addEventListener('pointermove', onPointerMove);
 
-    document.onpointerup = function (event) {
-      document.removeEventListener('pointermove', onPointerMove);
-      document.onpointerup = null;
-      if (placeHolder) {
-        placeHolder.before(li);
-        li.style.position = 'static';
+      document.onpointerup = function (event) {
+        document.removeEventListener('pointermove', onPointerMove);
+        document.onpointerup = null;
+        if (placeHolder) {
+          placeHolder.before(li);
+          li.classList.remove('sortable-list__item_dragging');
+          li.style = '';
 
-        placeHolder.remove();
-        placeHolder = null;
-      }
-    };
+          placeHolder.remove();
+          placeHolder = null;
+        }
+      };
 
-    function enterDroppable(elem) {
-      if (elem.getBoundingClientRect().top > placeHolder.getBoundingClientRect().top) {
-        elem.after(placeHolder);
-      } else {
-        elem.before(placeHolder);
+      function enterDroppable(elem) {
+        if (elem.getBoundingClientRect().top > placeHolder.getBoundingClientRect().top) {
+          elem.after(placeHolder);
+        } else {
+          elem.before(placeHolder);
+        }
       }
     }
   }
 
   onItemDeleteClick = (e) => {
-    console.log(e.target);
     if (e.target.dataset.deleteHandle === '') {
       const li = e.target.closest('li');
       if (li) {
@@ -100,12 +105,15 @@ export default class SortableList {
 
   render() {
     this.element = document.createElement('ul');
+    this.element.className = 'sortable-list';
 
     this.element.innerHTML = this.template;
 
     this.subElements = this.getSubElements(this.element);
 
     this.initEventListeners();
+
+    return this.element;
   }
 
   getSubElements(element) {
