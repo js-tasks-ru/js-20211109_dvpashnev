77f2317f533796f223ee1fc9b02d16ad02b98b85
Dimoon <dvpashnev@gmail.com>
2021-12-07 18:09:34 +0200

    9-2 task has been solved and tested

--- 09-tests-for-frontend-apps/2-sortable-list/index.js
+++ 09-tests-for-frontend-apps/2-sortable-list/index.js
@@ -2,103 +2,84 @@ export default class SortableList {
   items;
   subElements = {};
 
-  onMouseDown = (event) => {
+  onPointerDown = (event) => {
     const li = event.target.closest('.sortable-list__item');
     if (!li || !Object.values(this.subElements).find(el => el === li)) {
       return false;
     }
-    let currentDroppable = null;
+
+    let droppableBelow = null;
     const liBoundingClientRect = li.getBoundingClientRect();
 
     let placeHolder = document.createElement('div');
-    this.element.prepend(placeHolder);
     placeHolder.className = 'sortable-list__placeholder';
     placeHolder.style.width = liBoundingClientRect.width + 'px';
     placeHolder.style.height = liBoundingClientRect.height + 'px';
     placeHolder.style.top = liBoundingClientRect.top + 'px';
     placeHolder.style.left = liBoundingClientRect.left + 'px';
-    //placeHolder.style.position = 'absolute';
+    li.after(placeHolder);
 
     const shiftX = event.clientX - liBoundingClientRect.left;
     const shiftY = event.clientY - liBoundingClientRect.top;
     const width = liBoundingClientRect.width;
-    //console.log(li.getBoundingClientRect());
-    //li.style.position = 'absolute';
+    li.style.position = 'absolute';
     li.style.display = 'flex';
     li.style.width = width + 'px';
     li.style.zIndex = 10000;
-    //document.body.append(li);
 
     moveAt(event.pageX, event.pageY);
 
     function moveAt(pageX, pageY) {
-      //li.style.left = pageX - shiftX + 'px';
+      li.style.left = pageX - shiftX + 'px';
       li.style.top = pageY - shiftY + 'px';
     }
 
-    function onMouseMove(event) {
+    function onPointerMove(event) {
       moveAt(event.clientX, event.clientY);
       li.style.display = 'none';
       let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
       li.style.display = 'flex';
 
-      /*if (!elemBelow.classList.contains('sortable-list__item')) {
-        elemBelow = elemBelow.closest('li');
-      }*/
-
       if (!elemBelow) {
         return;
       }
 
-      let droppableBelow = elemBelow.closest('.sortable-list__item');
-      /*if (li === droppableBelow) {
-        droppableBelow = droppableBelow.closest('.sortable-list__item');
-      }*/
-      if (currentDroppable !== droppableBelow) {
-        if (currentDroppable) { // null если мы были не над droppable до этого события
-          // (например, над пустым пространством)
-          leaveDroppable(currentDroppable);
-        }
-        currentDroppable = droppableBelow;
-        if (currentDroppable) { // null если мы не над droppable сейчас, во время этого события
-          // (например, только что покинули droppable)
-          enterDroppable(currentDroppable);
-        }
+      droppableBelow = elemBelow.closest('.sortable-list__item');
+      if (droppableBelow) {
+        enterDroppable(droppableBelow);
       }
     }
 
-    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('pointermove', onPointerMove);
 
-    document.onmouseup = function () {
-      document.removeEventListener('mousemove', onMouseMove);
-      document.onmouseup = null;
+    document.onpointerup = function (event) {
+      document.removeEventListener('pointermove', onPointerMove);
+      document.onpointerup = null;
       if (placeHolder) {
-        li.style.left = placeHolder.getBoundingClientRect().left + 'px';
-        li.style.top = placeHolder.getBoundingClientRect().top + 'px';
+        placeHolder.before(li);
+        li.style.position = 'static';
+
         placeHolder.remove();
         placeHolder = null;
       }
     };
 
     function enterDroppable(elem) {
-      elem.after(placeHolder);
-
-      /*const left = elem.getBoundingClientRect().left;
-      const top = elem.getBoundingClientRect().top;
-      elem.style.left = placeHolder.getBoundingClientRect().left + 'px';
-      elem.style.top = placeHolder.getBoundingClientRect().top + 'px';
-      placeHolder.style.left = left + 'px';
-      placeHolder.style.top = top + 'px';*/
-
-      elem.style.background = 'pink';
+      if (elem.getBoundingClientRect().top > placeHolder.getBoundingClientRect().top) {
+        elem.after(placeHolder);
+      } else {
+        elem.before(placeHolder);
+      }
     }
+  }
 
-    function leaveDroppable(elem) {
-      /*if (placeHolder) {
-        placeHolder.remove();
-      }*/
-
-      elem.style.background = '';
+  onItemDeleteClick = (e) => {
+    console.log(e.target);
+    if (e.target.dataset.deleteHandle === '') {
+      const li = e.target.closest('li');
+      if (li) {
+        li.remove();
+      }
     }
   };
 
@@ -139,8 +120,9 @@ export default class SortableList {
   }
 
   initEventListeners() {
+    document.addEventListener('pointerdown', this.onItemDeleteClick);
     document.ondragstart = () => false;
-    document.addEventListener('mousedown', this.onMouseDown);
+    document.addEventListener('pointerdown', this.onPointerDown);
   }
 
   remove() {
@@ -149,5 +131,7 @@ export default class SortableList {
 
   destroy() {
     this.remove();
+    document.removeEventListener('pointerdown', this.onItemDeleteClick);
+    document.removeEventListener('pointerdown', this.onPointerDown);
   }
 }
